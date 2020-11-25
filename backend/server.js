d955be64e90d1b8df9cb89cc3e5b89b51e274d46
Cesar Gomez <zootechdrum@gmail.com>
2020-11-24 16:08:36 -0800

    added model for user

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,25 @@
-const express = require('express')
-const products = require('./data/products')
+import express from 'express'
+import dotenv from 'dotenv'
+import connectDB from './config/db.js'
+import products from './data/products.js'
 
+dotenv.config()
+connectDB()
 const app = express()
 
 app.get('/', (req, res) => {
   res.send('Api is running')
 })
-
+    
+cccsjdhfksjd
 app.get('/api/products', (req, res) => {
   res.json(products)
 })
-
 app.get('/api/products/:id', (req, res) => {
-  const product = products.find((p) => p._id === req.params.id)
+  product = products.find((p) => p._id === req.params.id)
   res.json(product)
 })
 
-app.listen(5000, console.log('Server is running on Port 5000'))
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, console.log(`Server is running on Port ${PORT}`))
