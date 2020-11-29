1c7e627fa209f9fe750c2630ac35ae56c3257a59
Cesar Gomez <zootechdrum@gmail.com>
2020-11-28 19:11:55 -0800

    moved models directory and added seeder user data

---
+++ backend/data/users.js
@@ -0,0 +1,23 @@
+import bcrypt from "bcryptjs";
+
+const users = [
+	{
+		name: "Admin User",
+		email: "zootechdrum@gmail.com",
+		password: bcrypt.hasSync("123456", 10),
+
+		isAdmin: true,
+	},
+	{
+		name: "John Doe",
+		email: "zoo@gmail.com",
+		password: bcrypt.hasSync("123456", 10),
+	},
+	{
+		name: "Jane Doe",
+		email: "drum@gmail.com",
+		password: bcrypt.hasSync("123456", 10),
+	},
+];
+
+export default users
