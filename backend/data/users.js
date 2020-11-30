e4d7d0377a31cb74043e7a3e7c736b4c19193f3b
Cesar Gomez <zootechdrum@gmail.com>
2020-11-29 20:14:25 -0800

    added conditional statement to insert or delete seeder data

--- backend/data/users.js
+++ backend/data/users.js
@@ -4,19 +4,19 @@ const users = [
 	{
 		name: "Admin User",
 		email: "zootechdrum@gmail.com",
-		password: bcrypt.hasSync("123456", 10),
+		password: bcrypt.hashSync("123456", 10),
 
 		isAdmin: true,
 	},
 	{
 		name: "John Doe",
 		email: "zoo@gmail.com",
-		password: bcrypt.hasSync("123456", 10),
+		password: bcrypt.hashSync("123456", 10),
 	},
 	{
 		name: "Jane Doe",
 		email: "drum@gmail.com",
-		password: bcrypt.hasSync("123456", 10),
+		password: bcrypt.hashSync("123456", 10),
 	},
 ];
 
--- backend/seeder.js
+++ backend/seeder.js
@@ -1,10 +1,10 @@
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import users from './data/users.js'
-import products from './data/users.js'
-import User from './data/userModel.js'
-import Product from './data/productModel.js'
-import Order from './data/orderModel.js'
+import products from './data/products.js'
+import User from './models/userModel.js'
+import Product from './models/productModel.js'
+import Order from './models/orderModel.js'
 import connectDB from './config/db.js'
 
 dotenv.config()
@@ -18,9 +18,39 @@ const importData = async() => {
     await User.deleteMany()
   
     const createdUsers = await User.insertMany(users)
+    const adminUser = createdUsers[0]._id
     const sampleProducts = products.map(product => {
+    
       return {...product, user:adminUser}
     })
+    await Product.insertMany(sampleProducts)
     console.log(sampleProducts)
-  }catch(error)
+    process.exit()
+  }catch(error){
+    console.log('Something went wront', error)
+  }
+
+}
+
+
+const destroyData = async() => {
+  try{
+    await Order.deleteMany()
+    await Product.deleteMany()
+    await User.deleteMany()
+    await Product.insertMany(sampleProducts)
+   
+    console.log(sampleProducts)
+    process.exit()
+  }
+  catch(error){
+    console.log('Something went wront', error)
+  }
+}
+
+if(process.argv[2] === '-d'){
+  destroyData()
+}else{
+importData()
+
 }
