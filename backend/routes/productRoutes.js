ee4e9a0d04d36e771cf9a57607c979b01a0bedea
Cesar Gomez <zootechdrum@gmail.com>
2020-12-13 13:42:47 -0800

    added additional routes to package.json file

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,18 @@
+import express from 'express'
+import asyncHandler from 'express-async-handler'
+const router = express.Router()
+import Product from '../models/productModel'
+
+
+app.get('/', async (req, res) => {
+
+  const products = await Product.find({})
+
+  res.json(products)
+})
+app.get('/:id', (req, res) => {
+  const product = products.find((p) => p._id === req.params.id)
+  console.log(product)
+  res.json(product)
+  
+})
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,10 @@ import dotenv from 'dotenv'
 import connectDB from './config/db.js'
 import products from './data/products.js'
 
+
+import productRoutes from './routes/productRoutes'
+
+
 dotenv.config()
 connectDB()
 const app = express()
@@ -11,16 +15,7 @@ app.get('/', (req, res) => {
   res.send('Api is running')
 })
 
-app.get('/api/products', (req, res) => {
-  res.json(products)
-})
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find((p) => p._id === req.params.id)
-  console.log(product)
-  res.json(product)
-  
-})
-
+app.use('/products', productRoutes)
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, console.log(`Server is running on Port ${PORT}`))
