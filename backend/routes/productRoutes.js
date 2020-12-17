a5e6468e9d4468d241ca660067656df1d35a4ab5
Cesar Gomez <zootechdrum@gmail.com>
2020-12-16 19:46:41 -0800

    added router for individual products

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -3,13 +3,22 @@ import asyncHandler from 'express-async-handler'
 const router = express.Router()
 import Product from '../models/productModel'
 
-app.get('/', async (req, res) => {
+app.get('/', asyncHandler(async (req, res) => {
     const products = await Product.find({})
 
     res.json(products)
-})
-app.get('/:id', (req, res) => {
-    const product = products.find((p) => p._id === req.params.id)
+}))
+app.get('/:id', asyncHandler( (req, res) => {
+    const product = await Product.findById(req.params.id)
+
+    if(product) {
+        res.json(product)
+
+    }else {
+        res.status(404).json({message: 'Product not found'})
+    }
     console.log(product)
     res.json(product)
-})
+}))
+
+export default router
